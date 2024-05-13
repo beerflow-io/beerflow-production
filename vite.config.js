78e9e1cbe634e30be7888bbc2849b09af876d96f
thefunkymonk13 <michael.reilly13@gmail.com>
2024-05-13 13:28:40 -0400

    message

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,36 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(() => {
+//   return {
+//     build: {
+//       outDir: 'build',
+//       assetsInclude: ['./src/amplifyconfiguration.json'],
+//       rollupOptions: {
+//         external: ['aws-amplify']
+//       }
+//     },
+//     resolve: {
+//         alias: {
+//           "./runtimeConfig": "./runtimeConfig.browser",
+//         },
+//     plugins: [react()],
+//   };
+// });
+
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-      assetsInclude: ['./src/amplifyconfiguration.json'],
-      rollupOptions: {
-        external: ['aws-amplify']
-      }
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Fix AWS Amplify issue where incorrect entry point is used
+  // Error: 'request' is not exported by __vite-browser-external, imported by node_modules/@aws-sdk/credential-provider-imds/dist/es/remoteProvider/httpRequest.js
+  // Github Issue: https://github.com/aws/aws-sdk-js/issues/3673
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
     },
-    resolve: {
-        alias: {
-          "./runtimeConfig": "./runtimeConfig.browser",
-        },
-    plugins: [react()],
-  };
+  },
 });
